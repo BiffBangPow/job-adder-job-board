5d58d6aad42064e84af91f1bb2e69973986dd35f
James Heathcote <james.h@biffbangpow.com>
2023-09-25 09:37:14 +0100

    Add local linkedin

--- src/DataObjects/JobConsultant.php
+++ src/DataObjects/JobConsultant.php
@@ -4,6 +4,7 @@ namespace BiffBangPow\JobAdderJobBoard\DataObjects;
 
 use BiffBangPow\JobAdderJobBoard\Extensions\JobAdderReferenceExtension;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\FieldType\DBVarchar;
 
@@ -35,6 +36,7 @@ class JobConsultant extends DataObject
     private static $db = [
         'FirstName' => DBVarchar::class,
         'LastName'  => DBVarchar::class,
+        'LinkedIn'  => DBVarchar::class,
         'Position'  => DBVarchar::class,
         'JobTitle'  => DBVarchar::class,
         'Email'     => DBVarchar::class,
@@ -56,6 +58,7 @@ class JobConsultant extends DataObject
     private static $summary_fields = [
         'FirstName'    => 'First Name',
         'LastName'     => 'Last Name',
+        'LinkedIn'     => 'LinkedIn',
         'JobAds.Count' => 'No Of Job Ads',
     ];
 
@@ -85,6 +88,19 @@ class JobConsultant extends DataObject
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
+
+        $fields->addFieldsToTab('Root.Main', [
+            TextField::create('FirstName')->setReadOnly(true),
+            TextField::create('LastName')->setReadOnly(true),
+            TextField::create('LinkedIn'),
+            TextField::create('Position')->setReadOnly(true),
+            TextField::create('JobTitle')->setReadOnly(true),
+            TextField::create('Email')->setReadOnly(true),
+            TextField::create('Phone')->setReadOnly(true),
+            TextField::create('Mobile')->setReadOnly(true),
+            TextField::create('PhotoURL')->setReadOnly(true),
+        ]);
+
         return $fields;
     }
 
@@ -116,15 +132,5 @@ class JobConsultant extends DataObject
     public function canView($member = null, $context = [])
     {
         return true;
-    }
-
-    /**
-     * @param null $member
-     * @param array $context
-     * @return bool
-     */
-    public function canEdit($member = null, $context = [])
-    {
-        return false;
     }
 }
